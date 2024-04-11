088cc533bfb95626557aa8001b756cbb3595976e
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-04-11 20:46:29 +0200

    layout

---
+++ layout/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+function displayFooter() {
+    $footer = "<footer>";
+    $footer .= "<p>&copy; 2024 E-commerce</p>";
+    $footer .= "</footer>";
+    return $footer;
+}
+
+echo displayFooter();
---
+++ layout/header.php
@@ -0,0 +1,4 @@
+<header class="navbar fixed-top bg-light shadow-sm d-flex justify-content-end align-items-start">
+    <?php require_once 'views/menu/user.php';?>
+    <?php require_once 'views/menu/navigation.php'; ?>
+</header>
