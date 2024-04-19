2c646f3a9ad7693f518828cc20e3bbbcc36beac5
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-04-19 18:53:19 +0200

    layout

--- layout/footer.php
+++ layout/footer.php
@@ -1,10 +1,31 @@
-<?php
-
-function displayFooter() {
-    $footer = "<footer>";
-    $footer .= "<p>&copy; 2024 E-commerce</p>";
-    $footer .= "</footer>";
-    return $footer;
-}
-
-echo displayFooter();
+<footer class="container container-fluid bg-light text-dark mt-3">
+    <div class="justify-content-around p-3 d-flex flex-row align-items-center justify-content-center">
+        <div class="">
+            <h3>Navigation</h3>
+            <ul>
+                <li><a href="index.php?page=home">Home</a></li>
+                <li><a href="index.php?page=products">Products</a></li>
+                <li><a href="index.php?page=contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="">
+            <h3>Legal</h3>
+            <ul>
+                <li><a href="index.php?page=terms">Terms</a></li>
+                <li><a href="index.php?page=privacy">Privacy</a></li>
+                <li><a href="index.php?page=cookies">Cookies</a></li>
+            </ul>
+        </div>
+        <div class="">
+            <h3>Follow us</h3>
+            <ul>
+                <li><a href="https://www.facebook.com">Facebook</a></li>
+                <li><a href="https://www.instagram.com">Instagram</a></li>
+                <li><a href="https://www.twitter.com">Twitter</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-flex flex-row align-items-center justify-content-center">
+        <p>&copy; 2024 - All rights reserved</p>
+    </div>
+</footer>
--- layout/header.php
+++ layout/header.php
@@ -1,4 +1,29 @@
-<header class="navbar fixed-top bg-light shadow-sm d-flex justify-content-end align-items-start">
-    <?php require_once 'views/menu/user.php';?>
-    <?php require_once 'views/menu/navigation.php'; ?>
+<header class="navbar fixed-top bg-light shadow-sm d-flex justify-content-end align-items-center justify-content-md-between ">
+
+    <div class="d-none d-md-block" style="width: 14rem;">
+        <a href="index.php">
+            <img src="assets/images/logo.png" alt="logo" class="logo">
+        </a>
+    </div>
+
+    
+    <div class="d-none d-xl-flex">
+        
+        <form id="search_form" action="controllers/search.php" method="POST">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="search" placeholder="À l'assaut de la perle rare">
+                            <button type="submit" class="btn btn-primary">
+                                <img src="assets/svg/search_heart.svg" width="40" height="40" alt="Rechercher">
+                            </button>
+                        </div>
+                    </form>
+                    <div id="search_error" class="text-danger"></div>
+                    <div id="search_result"></div>
+    </div>
+    <?php require_once 'views/menu/navigation.php'; ?>  
+    <div id="search_result" class="container shadow d-flex flex-wrap justify-content-center overflow-auto scrollbar" style="max-height: 30vh;"></div>
+    <div id="search_error" class="container shadow d-flex justify-content-center"></div>
+
+
 </header>
+
