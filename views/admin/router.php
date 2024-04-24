cf3285c914da41ee151cc0e858d1535fc6561490
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-04-24 18:29:29 +0200

    modif admin router

--- views/admin/router.php
+++ views/admin/router.php
@@ -3,36 +3,33 @@
     <?php 
     require_once 'views/menu/admin.php';
 
-
-    if(isset($_POST['admin_nav'])){
-        $value = $_POST['admin_nav'];
-        switch($value){
-            case 'dashboard':
-                require_once 'views/admin/dashboard.php';
-                break;
-            case 'users':
-                require_once 'views/admin/users/index.php';
-                break;
-            case 'products':
-                require_once 'views/admin/products/index.php';
-                break;
-            case 'orders':
-                require_once 'views/admin/orders/index.php';
-                break;
-            case 'settings':
-                require_once 'views/admin/settings/settings.php';
-                break;
-            case 'back':
-                $_SERVER['HTTP_REFERER'];
-                break;
-                
-            default:
-                require_once 'views/admin/dashboard.php';
-                break;
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['admin_nav'])){
+            $value = $_POST['admin_nav'];
+            switch($value){
+                case 'dashboard':
+                    require_once 'views/admin/dashboard.php';
+                    break;
+                case 'users':
+                    require_once 'views/admin/users/index.php';
+                    break;
+                case 'products':
+                    require_once 'views/admin/products/index.php';
+                    break;
+                case 'orders':
+                    require_once 'views/admin/orders/index.php';
+                    break;
+                case 'settings':
+                    require_once 'views/admin/settings/settings.php';
+                    break;
+                default:
+                    require_once 'views/admin/dashboard.php';
+                    break;
+            }
         }
     } else {
         require_once 'views/admin/dashboard.php';
     }
+    ?>
 
-     //require_once 'views/admin/router.php'; ?>
 </main>
