daeeb77a471d61b675524f5f7a77f3591baf434d
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-04-24 18:25:54 +0200

    search form

--- layout/header.php
+++ layout/header.php
@@ -7,22 +7,21 @@
     </div>
 
     
-    <div class="d-none d-xl-flex">
+    <div class=" d-xl-flex">
         
         <form id="search_form" action="controllers/search.php" method="POST">
-                        <div class="input-group">
-                            <input type="text" class="form-control" name="search" placeholder="À l'assaut de la perle rare">
-                            <button type="submit" class="btn btn-primary">
-                                <img src="assets/svg/search_heart.svg" width="40" height="40" alt="Rechercher">
-                            </button>
-                        </div>
-                    </form>
-                    <div id="search_error" class="text-danger"></div>
-                    <div id="search_result"></div>
+            <div class="input-group">
+                <input type="text" class="form-control" name="search" placeholder="À l'assaut de la perle rare">
+                <button type="submit" class="btn btn-primary">
+                    <img src="assets/svg/search_heart.svg" width="40" height="40" alt="Rechercher">
+                </button>
+            </div>
+        </form>
+
     </div>
     <?php require_once 'views/menu/navigation.php'; ?>  
     <div id="search_result" class="container shadow d-flex flex-wrap justify-content-center overflow-auto scrollbar" style="max-height: 30vh;"></div>
-    <div id="search_error" class="container shadow d-flex justify-content-center"></div>
+
 
 
 </header>
